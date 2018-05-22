a675c07ee56a2b54c095b26836dbad25b8cd5a17
Otavio Salvador <otavio@ossystems.com.br>
2018-05-22 16:21:41 -0300

    Reduce code in main and move it inside Jenkins module
    The code that acquires the information about the builds ought to be
    inside Jenkins module so main just uses this information.
    
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- src/jenkins.rs
+++ src/jenkins.rs
@@ -1,14 +1,18 @@
-use chrono::{DateTime, Duration, Utc};
+use chrono::{DateTime, Duration, TimeZone, Utc};
 use failure::Error;
+use jenkins_api::{Jenkins, JenkinsBuilder};
+use std::collections::BTreeMap;
 use std::env;
 
-pub struct Credentials {
-    pub username: String,
-    pub password: String,
-    pub server: String,
+use customer::{Customer, CustomerSet};
+
+struct Credentials {
+    username: String,
+    password: String,
+    server: String,
 }
 
-pub fn load_credentials_from_env() -> Result<Credentials, Error> {
+fn load_credentials_from_env() -> Result<Credentials, Error> {
     debug!("Loading credentials from environment");
     Ok(Credentials {
         server: env::var("JENKINS_SERVER")?,
@@ -17,8 +21,15 @@ pub fn load_credentials_from_env() -> Result<Credentials, Error> {
     })
 }
 
+pub fn connect() -> Result<Jenkins, Error> {
+    let credentials = load_credentials_from_env()?;
+    Ok(JenkinsBuilder::new(&credentials.server)
+        .with_user(&credentials.username, Some(&credentials.password))
+        .build()?)
+}
+
 #[derive(Debug, Clone)]
-pub(crate) struct JobInfo {
+pub struct JobInfo {
     pub name: String,
     pub builds: Vec<BuildInfo>,
 }
@@ -28,4 +39,71 @@ pub struct BuildInfo {
     pub number: u32,
     pub duration: Duration,
     pub timestamp: DateTime<Utc>,
+}
+
+pub fn get_jenkins_jobs_for_customers<'a>(
+    jenkins: &Jenkins,
+    customers: &'a CustomerSet,
+) -> Result<BTreeMap<&'a Customer, Vec<JobInfo>>, Error> {
+    let mut customer_use: BTreeMap<&Customer, Vec<JobInfo>> = BTreeMap::new();
+    for job in jenkins.get_home()?.jobs {
+        let customer_id = customers
+            .job_patterns()?
+            .matches(&job.name)
+            .into_iter()
+            .collect::<Vec<_>>()
+            .pop();
+
+        if customer_id.is_none() {
+            continue;
+        }
+
+        let customer = customers.get(customer_id.unwrap()).unwrap();
+        let job = job.get_full_job(&jenkins)?;
+        let builds = job
+            .builds()?
+            .into_iter()
+            .map(|build| -> Result<BuildInfo, Error> {
+                let build = build.get_full_build(&jenkins)?;
+                Ok(BuildInfo {
+                    number: build.number()?,
+                    timestamp: Utc.timestamp((build.timestamp()? / 1000) as i64, 0),
+                    duration: {
+                        let mut d = Duration::milliseconds(i64::from(build.duration()?));
+                        d = d + Duration::minutes(15 - (d.num_minutes() % 15));
+                        d
+                    },
+                })
+            });
+
+        let mut builds_info: Vec<BuildInfo> = Vec::new();
+        for build in builds {
+            match build {
+                Ok(b) => {
+                    if b.timestamp < Utc::now() - Duration::days(30) {
+                        break;
+                    }
+
+                    builds_info.push(b)
+                }
+
+                Err(e) => {
+                    error!("{:?}", e);
+                    continue;
+                }
+            };
+        }
+
+        let job = JobInfo {
+            name: job.name()?.into(),
+            builds: builds_info,
+        };
+
+        customer_use
+            .entry(customer)
+            .and_modify(|e| e.push(job.clone()))
+            .or_insert(vec![job]);
+    }
+
+    Ok(customer_use)
 }
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ extern crate jenkins_api;
 #[macro_use]
 extern crate serde_derive;
 extern crate regex;
+extern crate reqwest;
 extern crate serde;
 extern crate serde_yaml;
 extern crate stderrlog;
@@ -13,86 +14,17 @@ extern crate stderrlog;
 mod customer;
 mod jenkins;
 
-use customer::{Customer, CustomerSet};
-use jenkins::{BuildInfo, JobInfo};
-
-use chrono::{Duration, TimeZone, Utc};
-use jenkins_api::JenkinsBuilder;
-use std::collections::BTreeMap;
+use customer::CustomerSet;
 
 fn main() -> Result<(), failure::Error> {
     stderrlog::new().verbosity(1).init()?;
 
     info!("Starting Jenkins Builder Accounter");
 
-    let credentials = jenkins::load_credentials_from_env()?;
-    let jenkins = JenkinsBuilder::new(&credentials.server)
-        .with_user(&credentials.username, Some(&credentials.password))
-        .build()?;
 
     let customers = CustomerSet::load("config/customers.yml")?;
-    let job_patterns = customers.job_patterns()?;
-
-    let mut customer_use: BTreeMap<&Customer, Vec<JobInfo>> = BTreeMap::new();
-    for job in jenkins.get_home()?.jobs {
-        let customer_id = job_patterns
-            .matches(&job.name)
-            .into_iter()
-            .collect::<Vec<_>>()
-            .pop();
-
-        if customer_id.is_none() {
-            continue;
-        }
-
-        let customer = customers.get(customer_id.unwrap()).unwrap();
-        let job = job.get_full_job(&jenkins)?;
-        let builds = job
-            .builds()?
-            .into_iter()
-            .map(|build| -> Result<BuildInfo, failure::Error> {
-                let build = build.get_full_build(&jenkins)?;
-                Ok(BuildInfo {
-                    number: build.number()?,
-                    timestamp: Utc.timestamp((build.timestamp()? / 1000) as i64, 0),
-                    duration: {
-                        let mut d = Duration::milliseconds(i64::from(build.duration()?));
-                        d = d + Duration::minutes(15 - (d.num_minutes() % 15));
-                        d
-                    },
-                })
-            });
-
-        let mut builds_info: Vec<BuildInfo> = Vec::new();
-        for build in builds {
-            match build {
-                Ok(b) => {
-                    if b.timestamp < Utc::now() - Duration::days(30) {
-                        break;
-                    }
-
-                    builds_info.push(b)
-                }
-
-                Err(e) => {
-                    error!("{:?}", e);
-                    continue;
-                }
-            };
-        }
-
-        let job = JobInfo {
-            name: job.name()?.into(),
-            builds: builds_info,
-        };
-
-        customer_use
-            .entry(&customer)
-            .and_modify(|e| e.push(job.clone()))
-            .or_insert(vec![job]);
-    }
-
-    for (customer, jobs) in customer_use {
+    let jenkins = jenkins::connect()?;
+    for (customer, jobs) in jenkins::get_jenkins_jobs_for_customers(&jenkins, &customers)? {
         println!("Customer: {}", customer.name);
         for job in jobs {
             if job.builds.is_empty() {
